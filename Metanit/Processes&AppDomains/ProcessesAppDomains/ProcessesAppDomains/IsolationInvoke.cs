9d4d1e8cf100d20bc11ec0ee662bd8c247f4981a
Hatfieldx <tsyganov.nn@gmail.com>
2020-02-17 09:58:45 +0700

    learning processes / domains / contexsts (in dev)

---
+++ Metanit/Processes&AppDomains/ProcessesAppDomains/ProcessesAppDomains/IsolationInvoke.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Remoting;
+using System.Text;
+
+namespace ProcessesAppDomains
+{
+    class IsolationInvoke
+    {
+        public string LibPath { get; set; }
+
+        public string MethoName { get; set; }
+
+        public object[] Params { get; set; }
+
+        public IsolationInvoke()
+        {
+
+        }
+
+        public IsolationInvoke(string libpath, string method, object[] par)
+        {
+            Params = par;
+            MethoName = method;
+            LibPath = libpath;
+        }
+
+        public void InvokeMethod()
+        {
+            //validate directory
+            string paht = GetValidatePath();
+
+            AppDomain secondDomain = AppDomain.CreateDomain("Second domain");
+
+            Type LadaSedanType = Type.GetType("CarPro.LadaSedan, Cars");
+
+            ObjectHandle userHandle = secondDomain.CreateInstance("Cars", LadaSedanType.Name);
+
+            dynamic userProxy = userHandle.Unwrap();
+
+            if (RemotingServices.IsTransparentProxy(userProxy))
+            {
+
+            }
+
+
+        }
+
+        private string GetValidatePath()
+        {
+            if (File.Exists(LibPath))
+                return LibPath;
+
+            throw new FileNotFoundException($"file {LibPath} doesn't exist");
+        }
+    }
+}
---
+++ Metanit/Processes&AppDomains/ProcessesAppDomains/ProcessesAppDomains/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.Remoting;
+
+namespace ProcessesAppDomains
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process proc = Process.GetCurrentProcess();
+
+            foreach (ProcessThread item in proc.Threads)
+            {
+                //Console.WriteLine(item.Id);
+            }
+
+            (from currentprocess in Process.GetProcesses(".") orderby currentprocess.ProcessName select currentprocess)
+                .Distinct(new ProcessComparer())
+                .AsParallel()
+                .ForAll(x => Console.WriteLine(x.ProcessName));
+
+            var modules = proc.Modules.OfType<ProcessModule>();
+
+            (from m in modules orderby m.ModuleName select m)
+                .AsParallel()
+                .ForAll(x => Console.WriteLine(x.ModuleName));
+
+            Console.WriteLine(new string('=', 20) + " DOMAIN =>");
+
+            AppDomain defDomain = AppDomain.CurrentDomain;
+
+            Console.WriteLine(new string('=', 20) + " DOMAIN TEST EVENT LOAD ASSMS =>");
+
+            defDomain.AssemblyLoad += (ob, args) => Console.WriteLine(args.LoadedAssembly.GetName().Name);
+
+            Console.WriteLine(defDomain.FriendlyName);
+
+            Console.WriteLine(new string('=', 20) + " DOMAIN ASSMS =>");
+
+            var assms = from asm in defDomain.GetAssemblies() orderby asm.GetName().Name select asm;
+
+            foreach (var item in assms)
+            {
+                Console.WriteLine(item.GetName().Name);
+            }            
+
+            Console.ReadKey();
+        }
+
+        class ProcessComparer : IEqualityComparer<Process>
+        {
+            public bool Equals([AllowNull] Process x, [AllowNull] Process y)
+            {
+                return x.ProcessName == y.ProcessName;
+            }
+
+            public int GetHashCode([DisallowNull] Process obj)
+            {
+                return 0;
+            }
+        }
+
+    }
+}
